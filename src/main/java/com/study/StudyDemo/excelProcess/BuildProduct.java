2c3ec0fb11b68090a9e010d5a96497d888ea5b95
juweijia <juweijia@58.com>
2023-03-02 09:01:33 +0800

    add process excel

--- src/main/java/com/study/StudyDemo/excelProcess/BuildProduct.java
+++ src/main/java/com/study/StudyDemo/excelProcess/BuildProduct.java
@@ -6,14 +6,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BuildProduct {
-    public Map<String,String> sydc;
+//    public Map<String,String> sydc;
 //    public BuildProduct(){
 //        this.sydc=this.buildSydc();
 //    }
 //    @Test
     public Map<String,String> buildSydc(){
         Map<String,String> map= new HashMap<>();
-
+//商业地产
         map.put("用户增长","sydc");
         map.put("商业地产-经纪人服务","sydc");
         map.put("商业地产技术部","sydc");
@@ -22,13 +22,36 @@ public class BuildProduct {
         map.put("创新业务-数据服务(知楼）","sydc");
         map.put("创新业务—大底层建设(繁花）","sydc");
         map.put("创新业务-大业主","sydc");
-        map.put("","zf");
-        map.put("","ggzc");
-        map.put("","af");
-        map.put("","transaction");
-//        map.put("","sydc");
-//        map.put("","sydc");
-//        map.put("","sydc");
+//          租房
+        map.put("租房-租客体验","zf");
+        map.put("租房-品牌公寓","zf");
+        map.put("租房-运营活动","zf");
+        map.put("租房-房东组","zf");
+        map.put("租房-经纪组","zf");
+        map.put("前端-租房组","zf");
+        map.put("前端-公寓组","zf");
+        map.put("58GJ移动技术部","zf");
+        map.put("租房技术服务","zf");
+        map.put("58GJ数据策略部","zf");
+        map.put("租房","zf");
+
+//公共支持
+        map.put("前端技术部（北京侧）","ggzc");
+        map.put("基础平台-低代码平台","ggzc");
+        map.put("基础平台-基础服务（房源服务）","ggzc");
+        map.put("基础平台-房产开放平台","ggzc");
+        map.put("积木系统","ggzc");
+        map.put("前端-基础服务组","ggzc");
+        map.put("墨斗项目","ggzc");
+        map.put("天眼项目","ggzc");
+        map.put("58GJ质量保障部","ggzc");
+        map.put("58-online问题池","ggzc");
+
+
+
+        map.put("新房直销","af");
+        map.put("交易服务","jy");
+
 //        System.out.println(map);
         return map;
 
--- src/main/java/com/study/StudyDemo/excelProcess/ProcessExcel.java
+++ src/main/java/com/study/StudyDemo/excelProcess/ProcessExcel.java
@@ -23,35 +23,112 @@ public class ProcessExcel {
     @Test
     public void base(){
         BuildProduct buildProduct=new BuildProduct();
+//        大类别具体的数值
         int sydcNum=0;
+        int zfNum=0;
+        int ggzcNum=0;
+        int jyNum=0;
+        int afNum=0;
 
+//        创建一个map用来存储小类别具体的数值
         Map<String, Integer> sydcProduct=new HashMap<>();
+        Map<String, Integer> zfProduct=new HashMap<>();
+        Map<String, Integer> ggzcProduct=new HashMap<>();
+        Map<String, Integer> jyProduct=new HashMap<>();
+        Map<String, Integer> afProduct=new HashMap<>();
         try {
 //            读取excel文件到对象中
-            HSSFWorkbook book = new HSSFWorkbook(new FileInputStream("/Users/juju/Downloads/olog需求创建数.xls"));
-
+            HSSFWorkbook book = new HSSFWorkbook(new FileInputStream("/Users/juju/Downloads/ologW8.xls"));
 //            根据sheet名称获取表格页
-
 //            HSSFSheet table = book.getSheet("导出计数_业务线");
-            HSSFSheet table = book.getSheet("项目创建数据");
+            HSSFSheet table = book.getSheet("项目上线数据");
             for(int i=1; i<= table.getLastRowNum();i++){
 //                判断属于哪个大类
                 String content=table.getRow(i).getCell(3).getStringCellValue();
 //                System.out.println(content);
-                if("sydc".equals(buildProduct.buildSydc().get(content))){
-//                    System.out.println(buildProduct.buildSydc().get(content));
-                    sydcNum+=1;
-                    if(sydcProduct.containsKey(content)){
-                       int result=sydcProduct.get(content)+1;
-                        sydcProduct.remove(content);
-                        sydcProduct.put(content,result);
-                    }else{
-                        sydcProduct.put(content,1);
-                    }
+                try{
+                    buildProduct.buildSydc().get(content);
+                }catch (Exception exception){
+
                 }
+                switch (buildProduct.buildSydc().get(content)){
+                    case "sydc":
+                        sydcNum+=1;
+                        if(sydcProduct.containsKey(content)){
+                            int result=sydcProduct.get(content)+1;
+                            sydcProduct.remove(content);
+                            sydcProduct.put(content,result);
+                        }else{
+                            sydcProduct.put(content,1);
+                        }
+                        break;
+                    case "zf":
+                        zfNum+=1;
+                        if(zfProduct.containsKey(content)){
+                            int zftmp=zfProduct.get(content)+1;
+                            zfProduct.remove(content);
+                            zfProduct.put(content,zftmp);
+                        }else{
+                            zfProduct.put(content,1);
+                        }
+                        break;
+                    case "ggzc":
+                        ggzcNum+=1;
+                        if(ggzcProduct.containsKey(content)){
+                            int ggzctmp=ggzcProduct.get(content)+1;
+                            ggzcProduct.remove(content);
+                            ggzcProduct.put(content,ggzctmp);
+                        }else{
+                            ggzcProduct.put(content,1);
+                        }
+                        break;
+                    case "jy":
+                        jyNum+=1;
+                        if(jyProduct.containsKey(content)){
+                            int jytmp=jyProduct.get(content)+1;
+                            jyProduct.remove(content);
+                            jyProduct.put(content,jytmp);
+                        }else {
+                            jyProduct.put(content,1);
+                        }
+                        break;
+                    case "af":
+                        afNum+=1;
+                        if(afProduct.containsKey(content)){
+                            int aftmp=afProduct.get(content)+1;
+                            afProduct.remove(content);
+                            afProduct.put(content,aftmp);
+                        }else {
+                            afProduct.put(content,1);
+                        }
+                        break;
+                    default:
+                        System.out.println("content");
+                        break;
+
+                }
+//                if("sydc".equals(buildProduct.buildSydc().get(content))){
+////                    System.out.println(buildProduct.buildSydc().get(content));
+//                    sydcNum+=1;
+//                    if(sydcProduct.containsKey(content)){
+//                       int result=sydcProduct.get(content)+1;
+//                        sydcProduct.remove(content);
+//                        sydcProduct.put(content,result);
+//                    }else{
+//                        sydcProduct.put(content,1);
+//                    }
+//                }
             }
             System.out.println("商业地产总数量为"+sydcNum);
             System.out.println("各类目"+sydcProduct);
+            System.out.println("租房总数量为"+zfNum);
+            System.out.println("各类目"+zfProduct);
+            System.out.println("公共支持总数量为"+ggzcNum);
+            System.out.println("各类目"+ggzcProduct);
+            System.out.println("交易总数量为"+jyNum);
+            System.out.println("各类目"+jyProduct);
+            System.out.println("爱房总数量为"+afNum);
+            System.out.println("各类目"+afProduct);
 //            System.out.println(table.getFirstRowNum());
 //            System.out.println(table.getLastRowNum());
         } catch (IOException e) {
